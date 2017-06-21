a43de75f5b4129ae88e865f666635f92935bfb00
Võ Hoàng Quốc Tùng <quoctung1194@gmail.com>
2017-06-21 09:17:58 +0700

    Revert "Revert "Update Requirement""

---
+++ server/public/js/management/requirement/showList.js
@@ -0,0 +1,161 @@
+var isSubmited = false;
+
+$(document).ready(function() {
+    var table = $('#notificationTable').DataTable({
+        "processing": true,
+        "serverSide": true,
+        "ajax": $('#MR-004').val(),
+        "serverMethod": "GET",
+        "searching": false,
+        "order": [[ 1, "asc" ]],
+        "columns": [
+                    { "data": "stt", "targets"  : 'no-sort', "orderable": false},
+                    {
+                        "data": getItem,
+                        render: function(data) {
+                            var title = '';
+                            title += '<a href="' + $('#MR-002').val() + '/' + data.id + '">';
+                            title +=    data.title;
+                            title += '</a>';
+                            return title;
+                        }
+                    },
+                    {
+                        "data": "type_id",
+                        render: function(data, type, row) {
+                            return row.typeContent;
+                        }
+                    },
+                    {
+                        "data": "tag_id",
+                        render: function(data, type, row) {
+                            return row.tagContent;
+                        }
+                    },
+                    {
+                        "data": "user_id",
+                        render: function(data, type, row) {
+                            return row.account;
+                        }
+                    },
+                    { "data": "created_at", render: $.fn.dataTable.render.text() },
+                    { "data": getItem, "targets"  : 'no-sort', "orderable": false,
+                       render: function (data) {
+                            var icon = '';
+                            //Edit Icon
+                            icon += '<a href="' + $('#MR-002').val() + '/' + data.id + '">';
+                            icon += '   <i class="fa fa-pencil" aria-hidden="true" title="Edit"></i>';
+                            icon += '</a>';
+                            icon += '&nbsp;&nbsp;';
+
+                            //Remove Icon
+                            icon += '<a style="cursor:pointer" onclick="remove(' + data.id + ', this)">';
+                            icon += '   <i class="fa fa-trash" aria-hidden="true" title="Remove"></i>';
+                            icon += '</a>';
+                            icon += '&nbsp;&nbsp;';
+
+                            //Lock Icon
+                            icon += '<a style="cursor:pointer" onclick="lock(' + data.id + ', this)">';
+                            if(data.locked == 0) {
+                                icon += '<i class="fa fa-unlock" aria-hidden="true" title="Unlocked"></i>';
+                            } else {
+                                icon += '<i class="fa fa-lock" aria-hidden="true" title="Locked"></i>';
+                            }
+                            icon += '</a>';
+                            icon += '&nbsp;&nbsp;';
+
+                            return icon;
+                       }
+                    }
+                ]
+    });
+});
+
+function getItem(data) {
+    return data;
+}
+
+function lock(id, button) {
+    if(isSubmited){
+        return;
+    }
+
+    var accepted = confirm("Bạn có chắc muốn khóa không ?");
+    if(!accepted) {
+        return;
+    }
+    debugger;
+    var formData = new FormData();
+    formData.append('_token', $('#csrf-token').val());
+    formData.append('id', id);
+
+    isSubmited = true;
+
+    $.ajax({
+        url: $('#MR-006').val(),
+        type: 'POST',
+        contentType: false,
+        cache: false,
+        data: formData,
+        processData: false,
+        success: function(data) {
+            if(data.success) {
+                icon = button.childNodes[0]
+                if(data.locked) {
+                    icon.setAttribute('class', 'fa fa-lock');
+                    icon.setAttribute('title', 'Locked');
+                } else {
+                    icon.setAttribute('class', 'fa fa-unlock');
+                    icon.setAttribute('title', 'Unlocked');
+                }
+            }
+        },
+        error: function(data) {
+            alert('Lỗi! Vui lòng thử lại');
+        },
+        complete: function( data ) {
+            isSubmited = false;
+        }
+    });
+}
+
+function remove(id, button) {
+    if(isSubmited){
+        return;
+    }
+
+    var accepted = confirm("Bạn có chắc muốn xóa không ?");
+
+    if(!accepted) {
+        return;
+    }
+
+    var formData = new FormData();
+    formData.append('_token', $('#csrf-token').val());
+    formData.append('id', id);
+
+    isSubmited = true;
+
+    $.ajax({
+        url: $('#MR-005').val(),
+        type: 'POST',
+        contentType: false,
+        cache: false,
+        data: formData,
+        processData: false,
+        success: function(data) {
+            if(data.success) {
+                //Lay dòng data hiện hành
+                var currentRow = button.parentNode.parentNode;
+                //Xóa dòng data hiện hành
+                currentRow.parentNode.removeChild(currentRow);
+            }
+        },
+        error: function(data) {
+            alert('Lỗi! Vui lòng thử lại');
+        },
+        complete: function( data ) {
+            isSubmited = false;
+        }
+    });
+}
--- server/resources/views/managements/requirements/showList.blade.php
+++ server/resources/views/managements/requirements/showList.blade.php
@@ -1,51 +1,42 @@
+<?php 
+use App\Constants\CommonConstant;
+?>
+
 @extends('managements.master')
 
 @section('content')
+<!-- <link rel="stylesheet" href="{{ URL::asset('resources/dataTables/jquery.dataTables.min.css?v=' . CommonConstant::RESOURCE_VERSION) }}"> -->
+<link rel="stylesheet" href="{{ URL::asset('resources/datatables/dataTables.bootstrap.css?v=' . CommonConstant::RESOURCE_VERSION) }}">
+<link rel="stylesheet" href="{{ URL::asset('resources/fontAwesome/css/font-awesome.css?v=' . CommonConstant::RESOURCE_VERSION) }}">
+
+<script src="{{ URL::asset('resources/datatables/jquery.dataTables.min.js?v=' . CommonConstant::RESOURCE_VERSION) }}"></script>
+<script src="{{ URL::asset('resources/datatables/dataTables.bootstrap.min.js?v=' . CommonConstant::RESOURCE_VERSION) }}"></script>
+<script src="{{ URL::asset('js/management/requirement/showList.js?v=' . CommonConstant::RESOURCE_VERSION) }}"></script>
+
+<input type="hidden" id="MR-002" value="{{ route('MR-002') }}" />
+<input type="hidden" id="MR-004" value="{{ route('MR-004') }}" />
+<input type="hidden" id="MR-005" value="{{ route('MR-005') }}" />
+<input type="hidden" id="MR-006" value="{{ route('MR-006') }}" />
+<input type="hidden" id="csrf-token" value="{{ csrf_token() }}" />
+
 <h1 class="page-header">@lang('main.requirement')</h1>
-<div class="row placeholders">
-	<div class="col-xs-6 col-sm-3 placeholder">
-		
-	</div>
-	<div class="col-xs-6 col-sm-3 placeholder"></div>
-	<div class="col-xs-6 col-sm-3 placeholder"></div>
-	<div class="col-xs-6 col-sm-3 placeholder"></div>
+<div style="margin-bottom: 20px" >
+    <a href="{{ route('MR-002') }}" type="button" class="btn btn-default buttonCreate">@lang('main.add_new')</a>
 </div>
 <h2 class="sub-header">@lang('main.requirement_list')</h2>
-<div class="table-responsive">
-	<table id="notificationTable" class="table table-striped">
+<div>
+	<table id="notificationTable" class="table" style="width: 100%;">
 		<thead>
 			<tr>
-				<th>#</th>
-				<th>@lang('main.title')</th>
-				<th>@lang('main.type')</th>
-				<th>@lang('main.tag')</th>
-				<th>@lang('main.username')</th>
-				<th>@lang('main.created_at')</th>
-				<th>@lang('main.action')</th>
+				<th width="10%">#</th>
+				<th width="30%">@lang('main.title')</th>
+				<th width="10%">@lang('main.type')</th>
+				<th width="10%">@lang('main.tag')</th>
+				<th width="15%">@lang('main.username')</th>
+				<th width="15%">@lang('main.created_at')</th>
+				<th width="10%">@lang('main.action')</th>
 			</tr>
 		</thead>
-		<tbody>
-		@foreach ($requirements as $requirement)
-			<tr>
-				<td>{{ $requirement->id }}</td>
-				<td>{{ $requirement->title }}</td>
-				<td>{{ $requirement->typeContent }}</td>
-				<td>{{ $requirement->tagContent }}</td>
-				<td>{{ $requirement->account }}</td>
-				<td>{{ $requirement->created_at }}</td>
-				<td>
-					<img src="http://tiko.vn/design/standard/images/copy.gif"
-						alt="Copy">&nbsp;
-					<a href="{{ route('MR-002', ['id' => $requirement->id]) }}">
-						<img src="http://tiko.vn/design/standard/images/edit.gif" alt="Edit">&nbsp;
-					</a>
-					<img
-						src="https://cdn2.iconfinder.com/data/icons/25-free-ui-icons/40/trash_bin-16.png"
-						alt="bin, delete, remove, trash, trash bin, trash can icon">&nbsp;
-				</td>
-			</tr>
-		@endforeach
-		</tbody>
 	</table>
 </div>
 @endsection
